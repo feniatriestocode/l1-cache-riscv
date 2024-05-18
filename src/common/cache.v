65a920a83ffe3818b89c88637f0d59bd4766471c
Fenia Papadouli <gapapadouli@gmail.com>
2024-05-18 13:23:55 +0300

    write back to the cache -- minor changes

--- src/common/cache.v
+++ src/common/cache.v
@@ -20,17 +20,9 @@ module cache
         input wire rden, 
         input wire [LINE_WIDTH:0] addr,
         input wire [LINE_WIDTH:0] data_in,
-        input wire [LINE_WIDTH:0] mem_in,
-gi
         output wire cache_hit,
-        output wire [LINE_WIDTH:0] data_out, //should that be a reg?
-        output wire [LINE_WIDTH-1:0] mem_out
-
-        //more mem related outputs
-        output wire [LINE_WIDTH-1:0] memrd_addr,
-        output wire [LINE_WIDTH-1:0] memwr_addr,
-        output wire memrd_en,
-        output wire memwr_en
+        output wire [LINE_WIDTH:0] data_out,
+        output wire [LINE_WIDTH:0] mem_out
     );
 
     //segmentation of the table
@@ -60,16 +52,16 @@ gi
     always @(posedge clk) 
         begin  
             //extract the tag, index and offset
-            tag = addr[TAG_WIDTH+INDEX_WIDTH+OFFSET_WIDTH:INDEX_WIDTH+OFFSET_WIDTH]; //32:13
-            index = addr[INDEX_WIDTH+OFFSET_WIDTH:OFFSET_WIDTH]; //12:3
-            offset = addr[OFFSET_WIDTH:0]; //2:0
+            tag <= addr[TAG_WIDTH+INDEX_WIDTH+OFFSET_WIDTH:INDEX_WIDTH+OFFSET_WIDTH]; //32:13
+            index <= addr[INDEX_WIDTH+OFFSET_WIDTH:OFFSET_WIDTH]; //12:3
+            offset <= addr[OFFSET_WIDTH:0]; //2:0
             if (rden) 
             begin
                 //check for value in cache
-                cache_hit = 0;
-                for (int i = 0; i < NUM_LINES; i++)
+                cache_hit <= 0;
                 begin
                     if (valid[i][index] && tag[i][index] == tag) 
+
                     begin
                         cache_hit <= 1;
                         data_out <= data[i][index][offset];
@@ -81,7 +73,7 @@ gi
             else if (wren)
             begin
                 //write to cache
-                cache_hit = 0;
+                cache_hit <= 0;
                 for (int i = 0; i < NUM_LINES; i++)
                 begin
                     if (valid[i][index] && tag[i][index] == tag) 
@@ -97,13 +89,12 @@ gi
                 begin
                     if (dirty[i][index])
                     begin
-                        //write back to mem
+                        mem_out <= data[i][index][offset];
                     end
                     //update cache line
                     valid[i][index] <= 1;
                     data[i][index][offset] <= data_in;
                     tag[i][index] <= tag;
-
                 end 
             end
         end
