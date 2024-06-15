a0cfb4676ea009573e2d31d74aac43d889925852
AnnaGais <118528342+AnnaGais@users.noreply.github.com>
2024-06-15 12:00:08 +0300

    dcache

--- src/caches/D_SRAM.v
+++ src/caches/D_SRAM.v
@@ -13,119 +13,175 @@ module D_SRAM(clk, rst, en, wen, dmemWen, bytesAccess, blockAddr, dataIn, hit, d
     //INPUT PORTS//
     input clk, rst;
     input en, wen, dmemWen;
-    input [`BLOCK_BYTES_NUM-1:0] bytesAccess;
-    input [`TAG_BITS+`SET_INDEX_BITS:0] blockAddr;
-    input [`BLOCK_SIZE_BITS-1:0] dataIn;
+    input [`DCACHE_BLOCK_SIZE-1:0] bytesAccess;
+    input [`DTAG_SIZE+`DINDEX_SIZE:0] blockAddr;
+    input [`DBLOCK_SIZE_BITS-1:0] dataIn;
     //OUTPUT PORTS//
     output wire hit;
     output wire dirtyBit;
-    output wire [`BLOCK_SIZE_BITS-1:0] dataOut;
+    output wire [`DBLOCK_SIZE_BITS-1:0] dataOut;
 
     //SRAM
-    reg [`ASSOCIATIVITY-1:0]   valid_col  [`BLOCK_NUM-1:0];
-    reg [`ASSOCIATIVITY-1:0]   dirty_col  [`BLOCK_NUM-1:0];
-    reg [`ASSOCIATIVITY-1:0]   status_col [`BLOCK_NUM-1:0]; 
-    reg [`TAG_BITS-1:0]        tag_col    [`BLOCK_NUM-1:0][`ASSOCIATIVITY-1:0];
-    reg [`BLOCK_SIZE_BITS-1:0] data_col   [`BLOCK_NUM-1:0][`ASSOCIATIVITY-1:0];
+    reg [`DASSOCIATIVITY-1:0]   valid_col  [`DBLOCK_NUMBER-1:0];
+    reg [`DASSOCIATIVITY-1:0]   dirty_col  [`DBLOCK_NUMBER-1:0];
+    reg [`DASSOCIATIVITY-1:0]   status_col [`DBLOCK_NUMBER-1:0]; 
+    reg [`DTAG_SIZE-1:0]        tag_col    [`DBLOCK_NUMBER-1:0][`DASSOCIATIVITY-1:0];
+    reg [`DBLOCK_SIZE_BITS-1:0] data_col   [`DBLOCK_NUMBER-1:0][`DASSOCIATIVITY-1:0];
 
     //ADDRESS' SEGS
     wire [`BLOCK_OFFSET_BITS-1:0] offset;
-    wire [`SET_INDEX_BITS-1:0] index;
-    wire [`TAG_BITS-1:0] tag;
+    wire [`DINDEX_SIZE-1:0] index;
+    wire [`DTAG_SIZE-1:0] tag;
     
     //BLOCK ADDRESS = [TAG][INDEX]
-    assign index = addr[`SET_INDEX_BITS-1:0];
-    assign tag   = addr[`TAG_BITS+`SET_INDEX_BITS-1:`SET_INDEX_BITS]; 
+    assign index = blockAddr[`DINDEX_SIZE-1:0];
+    assign tag   = blockAddr[`DTAG_SIZE+`DINDEX_SIZE-1:`DINDEX_SIZE]; 
 
-    //******************************ASYCHRONOUS HIT*********************************//
+    //*****************************RESET INITIALIZATION*****************************//
+    integer l;
+    always @(posedge clk or negedge rst)begin
+        if(!rst)
+            for(l=0; l<`DBLOCK_NUMBER; l=l+1)begin
+                valid_col [l] = {`DASSOCIATIVITY{1'b0}};
+                status_col[l] = {`DASSOCIATIVITY{1'b0}};
+                dirty_col [l] = {`DASSOCIATIVITY{1'b0}};
+            end
+    end
+
+    //******************************ASYCHRONOUS HIT********************************//
     integer j;
-    reg [`ASSOCIATIVITY-1:0] hit_reg;
+    reg [`DASSOCIATIVITY-1:0] hitReg;
 
     always @(addr or en or rst)begin
-        for(j=0; j<`ASSOCIATIVITY; j++)begin
+        for(j=0; j<`DASSOCIATIVITY; j++)begin
             if(en && rst)
-                hit_reg[j] = (tag_col[index][j] == tag) && valid_col[index][j];
+                hitReg[j] = (tag_col[index][j] == tag) && valid_col[index][j];
             else
-                hit_reg[j] = {`ASSOCIATIVITY{1'b0}};
+                hitReg[j] = {`DASSOCIATIVITY{1'b0}};
         end
     end
 
-    assign hit = |hit_reg;
+    assign hit = |hitReg;
 
-    //****************************SYNCHRONOUS READ DATA****************************//
+    //****************************ASYNCHRONOUS READ DATA***************************//
 
     integer i;
-    reg [`BLOCK_SIZE_BITS-1:0] dataOut_reg;
+    reg [`DBLOCK_SIZE_BITS-1:0] dataOut_reg;
 
     always @(hit or en or rst)begin
-        dataOut_reg = {`BLOCK_SIZE_BITS{1'b0}};
+        dataOut_reg = {`DBLOCK_SIZE_BITS{1'b0}};
 
-        for(i=0; i<`ASSOCIATIVITY; i=i+1)
-            if(en && rst && hit_reg[i] == 1'b1)begin
+        for(i=0; i<`DASSOCIATIVITY; i=i+1)
+            if(en && rst && hitReg[i] == 1'b1)begin
                 dataOut_reg = data_col[index][i];
             end
     end
 
     assign dataOut = dataOut_reg;
 
-    //***************************SYNCHRONOUS WRITE DATA***************************//
-    integer m,k,l;
+    //************************ΑSYNCHRONOUS READ DIRTY BIT*************************//
+
     reg dirtyBit_reg;
     reg found;
+    reg [`DASSOCIATIVITY-1:0] blockToEvict;
+    integer w;
 
-    always @(posedge clk or negedge rst)begin
-        if(!rst)begin
-            dirtyBit_reg <= 1'b0;
-        end
-        else begin
-            if(wen && hit)
-                for(m=0; m<`ASSOCIATIVITY; m=m+1)begin
-                    if(hit_reg[m])begin
-                        valid_col [index][m]  <= 1'b1;
-                        dirty_col [index][m]  <= 1'b1;
-                        status_col[index][m]  <= 1'b1;
-                        tag_col   [index][m]  <= tag;
+    always @(hit or en or rst)begin
+        dirtyBit_reg = 1'b0;
+        found = 1'b0;
+        blockToEvict = {`DASSOCIATIVITY{1'b0}};
 
-                        //PLRU POLICY
-                        if(&status_col)begin
-                            for(k=0; k<`ASSOCIATIVITY; k=k+1)
-                                if(k != m)
-                                    status_col[index][k] <= 1'b0;
-                        end
-                        
-                        //CACHE WRITE BASED ON WHICH BYTE WE WANT TO ACCESS
-                        for(l=0; l<`BLOCK_BYTES_NUM; l=l+1)begin
-                            if(bytesAccess[l])
-                                data_col[index][m][(l*8)+:8] = dataIn[(l*8)+:8];
-                        end
-
-                    end
+        if(en && rst && !hit)
+            for(w=0; w<`DASSOCIATIVITY; w=w+1)
+                if(status_col[index][w] == 1'b0 && found == 1'b0)begin
+                    found = 1'b1;
+                    blockToEvict[w] = 1'b1;
+                    dirtyBit_reg = dirty_col[index][w]; 
                 end
-            else if(wen && !hit)begin
-                found <= 0;
-                for(m=0; m<`ASSOCIATIVITY; m=m+1)
-                    if(!status_col[index][m] && !found)begin
-                        dirtyBit_reg <= dirty_col[index][m];
-                        found = 1'b1;
-                    end
-
-                    if(dmemWen)begin
-                        dirty_col[index][m] <= 1'b0;
-                        valid_col[index][m] <= 1'b1;
-                        status_col[index][m] <= 1'b1;
-                        tag_col[index][m] <= tag;
-                        data_col[index][m] <= dataIn;
-
-                        if(&status_col)begin
-                            for(k=0; k<`ASSOCIATIVITY; k=k+1)
-                                if(k != m)
-                                    status_col[index][k] <= 1'b0;
-                        end
-                    end
-            end
-        end
     end
 
     assign dirtyBit = dirtyBit_reg;
+
+    //***************************SYNCHRONOUS WRITE DATA***************************//
+    reg wen_pl;
+    reg en_pl;
+    reg dMemWen_pl;
+    reg hit_pl;
+    reg [`DASSOCIATIVITY-1:0]   blockToEvict_pl;
+    reg [`DASSOCIATIVITY-1:0]   hitReg_pl;
+    reg [`DINDEX_SIZE-1:0]      index_pl;
+    reg [`DTAG_SIZE-1:0]        tag_pl;
+    reg [`DBLOCK_SIZE_BITS-1:0] dataIn_pl;
+
+    integer m,l,k;
+
+    always @(posedge clk or negedge rst)begin
+        if(!rst)begin
+            wen_pl <= wen;
+            en_pl <= en;
+            hit_pl <= hit;
+            hit_reg_pl <= {`DASSOCIATIVITY{1'b0}};
+            index_pl <= {`DINDEX_SIZE{1'b0}};
+            tag_pl <= {`DTAG_SIZE{1'b0}};
+            blockToEvict_pl <= {`DASSOCIATIVITY{1'b0}};
+            dataIn_pl <= {`DBLOCK_SIZE_BITS{1'b0}};
+        end
+        else begin
+            wen_pl <= wen;
+            hit_pl <= hit;
+            hitReg_pl <= hit_reg;
+            dMemWenpl <= dmemWen;
+            index_pl <= index;
+            blockToEvict_pl <= blockToEvict;
+            tag_pl <= tag;
+            en_pl <= en;
+            dataIn_pl <= dataIn;
+            
+            if(en_pl)begin //CACHE ENABLED
+                if(wen_pl)begin //CACHE IN WRITE MODE
+                    if(hit_pl)begin //BLOCK HIT
+                        for(m=0; m<`DASSOCIATIVITY; m=m+1)begin
+                            if(hitReg[m])begin
+                                valid_col [index_pl][m]  <= 1'b1;
+                                dirty_col [index_pl][m]  <= 1'b1;
+                                status_col[index_pl][m]  <= 1'b1;
+                                tag_col   [index_pl][m]  <= tag_pl;
+                                
+                                //PLRU POLICY
+                                if(&status_col)begin
+                                    for(k=0; k<`DASSOCIATIVITY; k=k+1)
+                                        if(k != m)
+                                            status_col[index_pl][k] <= 1'b0;
+                                end
+                            
+                                //CACHE WRITE BASED ON WHICH BYTE WE WANT TO ACCESS
+                                for(l=0; l<`DCACHE_BLOCK_SIZE; l=l+1)
+                                    if(bytesAccess[l])
+                                        data_col[index_pl][m][(l*8)+:8] = dataIn[(l*8)+:8];
+                            end
+                        end
+                    end 
+                    else if(dMemWen_pl)begin 
+                        for(m=0; m<`DASSOCIATIVITY; m=m+1)
+                            if(blockToEvict_pl[m] == 1'b1)begin
+                                dirty_col [index_pl][m] <= 1'b0;
+                                valid_col [index_pl][m] <= 1'b1;
+                                status_col[index_pl][m] <= 1'b1;
+                                tag_col   [index_pl][m] <= TAG_PL;
+                                data_col  [index_pl][m] <= DATAIN_PL;
+                            end
+
+                        //PLRU POLICY
+                        if(&status_col)
+                            for(k=0; k<`DASSOCIATIVITY; k=k+1)
+                                if(k != m)
+                                    status_col[index_pl][k] <= 1'b0;
+                    end
+
+                end //end of WEN_PL if
+            end //end of EN_PL if
+        end //end of rst==1 if
+    end //end of always
+
 
 endmodule
--- src/caches/D_SRAM_tb.v
+++ src/caches/D_SRAM_tb.v
@@ -1,37 +1,34 @@
 `timescale 1ns/1ps
 
 module D_SRAM_tb;
-    //INPUT PORTS//
     reg clk, rst;
     reg en, wen, dmemWen;
-    reg [`BLOCK_BYTES_NUM-1:0] bytesAccess;
-    reg [`ADDR_SIZE-1:0] addr;
-    reg [`BLOCK_SIZE_BITS-1:0] dataIn;
-    //OUTPUT PORTS//
+    reg [`DCACHE_BLOCK_SIZE-1:0] bytesAccess;
+    reg [`DTAG_SIZE+`DINDEX_SIZE:0] blockAddr;
+    reg [`DBLOCK_SIZE_BITS-1:0] dataIn;
+
     wire hit;
     wire dirtyBit;
-    wire [`BLOCK_SIZE_BITS-1:0] dataOut;
+    wire [`DBLOCK_SIZE_BITS-1:0] dataOut;
 
-    D_SRAM D_SRAM_inst(clk, rst, en, wen, dmemWen, bytesAccess, addr, dataIn, hit, dirtyBit, dataOut);
-    
-    initial begin 
+    D_SRAM D_SRAM_inst(.clk(clk), .rst(rst), .en(en), .wen(wen), .dmemWen(dmemWen), .bytesAccess(bytesAccess), 
+                       .blockAddr(blockAddr), .dataIn(dataIn), .hit(hit), .dirtyBit(dirtyBit), .dataOut(dataOut));
+
+    initial begin
         clk=0;
-        rst=0;
-        #20 rst=1;
-        #10 adrr=0;
-            wen=0;
-            en=0;
-            
+        reset=0
+        #20 reset=1
+        #10 en=1;
+            wen=1;
+
+
 
 
     end
-    
+
     always begin
         #5 clk = ~clk;
     end
 
-    initial begin
-        #60000 $finish;
-    end
-    
+
 endmodule
