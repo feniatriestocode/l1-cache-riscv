6b9f4c8506df2a369032b056d0bab68b4f30a35f
Fenia Papadouli <gapapadouli@gmail.com>
2024-06-15 16:34:23 +0300

    Added testing config doc

--- testbench/obj_dir/Vtoplevel__ALL.d
+++
@@ -1,9 +0,0 @@
-Vtoplevel__ALL.o: Vtoplevel__ALL.cpp Vtoplevel.cpp Vtoplevel.h \
- /usr/share/verilator/include/verilated_heavy.h \
- /usr/share/verilator/include/verilated.h \
- /usr/share/verilator/include/verilatedos.h Vtoplevel__Syms.h \
- Vtoplevel___024unit.h Vtoplevel___024unit.cpp Vtoplevel__Trace.cpp \
- /usr/share/verilator/include/verilated_vcd_c.h \
- /usr/share/verilator/include/verilated_trace.h Vtoplevel__Slow.cpp \
- Vtoplevel___024unit__Slow.cpp Vtoplevel__Syms.cpp \
- Vtoplevel__Trace__Slow.cpp
