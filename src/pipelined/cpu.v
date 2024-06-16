8ef3a9809fae9177cc97d2633952a568e6c93f80
kananou <kallioph.n@gmail.com>
2024-06-16 15:07:36 +0300

    pipeline changes

--- src/pipelined/cpu.v
+++ src/pipelined/cpu.v
@@ -2,15 +2,113 @@
 `include "config.vh"
 
 module cpu(input clock, 
-		   input reset, 
-		   output overflow);
+		   input reset,
+		   // imem
+		   output imem_ren,
+           output [($clog2(`MEM_SIZE)-1):0] imem_block_address,
+           input [((`WORD_SIZE*`BLOCK_SIZE)-1):0] imem_dout,
+           input imem_ready,
+           // dmem
+           output dmem_ren, 
+           output dmem_wen,
+           output [($clog2(`MEM_SIZE)-1):0] dmem_block_address, 
+           output [((`WORD_SIZE*`BLOCK_SIZE)-1):0] dmem_din,
+           input dmem_ready, 
+           input dmem_done,
+           input [((`WORD_SIZE*`BLOCK_SIZE)-1):0] dmem_dout);     
+           
+           
+ //pipeline          
+wire dcache_stall, dcache_ren, dcache_wen;
+wire [] dcache_output;
+wire [] dcache_addr;
+wire [] byteSelectVector;
+wire [] dcache_input;
+
+// dcache
+wire DcacheEn, DcacheWen, DcacheMemWen;
+wire [] DcacheBytesAccess;
+wire [] DcacheBlockAddr;
+wire [] DcacheDin;
+wire DcacheHit, DcacheDirtyBit;
+wire [] DcacheDout;
+
+
+// icache
+wire IcacheEn, IcacheMemWen;
+wire [] IcacheBlockAddr;
+wire [] IcacheDin;
+wire IcacheHit, IcacheDirtyBit;
+wire [] IcacheDout;
 
 pipeline pipeline(.clock(clock), 
-				  .reset(reset), 
-				  .overflow(overflow));
+				  .reset(reset),
+		/*input*/ .dcache_stall(dcache_stall),
+                  .dcache_output(dcache_output),
+				  .dcache_ren(dcache_ren),
+				  .dcache_wen(dcache_wen),
+				  .dcache_addr(dcache_addr),
+				  .byteSelectVector(byteSelectVector),
+				  .dcache_input(dcache_input));
 
 // caches instantiated here
 // to be implemented !!!!!!!!!!!!!!!!!!!!!
-Icache Icache();
-Dcache Dcache();
+Icache_SRAM( Icache(.clk(clock), 
+                .rst(reset),
+                .en(IcacheEn), 
+                .memWen(IcacheMemWen),
+                .blockAddr(IcacheBlockAddr), .
+                .dataIn(IcacheDin), .
+                .hit(IcacheHit),
+                .dirtyBit(IcacheDirtyBit),
+                .dataOut(IcacheDout));
+
+D_SRAM Dcache(.clk(clock), 
+                .rst(reset),
+                .en(DcacheEn), 
+                .wen(DcacheWen), 
+                .dmemWen(DcacheMemWen),
+                .bytesAccess(DcacheBytesAccess),
+                .blockAddr(DcacheBlockAddr), .
+                .dataIn(DcacheDin), .
+                .hit(DcacheHit),
+                .dirtyBit(DcacheDirtyBit),
+                .dataOut(DcacheDout));
+
+dcache_controller(// pipeline inputs
+                        .clock(clock,
+                        .reset(reset),
+                        .ren(dcache_ren), 
+                        .wen(dcache_wen),
+                        .addr(dcache_addr),
+                        .byteSelectVector(byteSelectVector),
+                        .din(dcache_input),
+                        
+                        // cache inputs
+                        .cacheHit(DcacheHit),
+                        .cacheDirtyBit(DcacheDirtyBit),
+                        .cacheDout(DcacheDout),
+                        
+                        // memory inputs
+                        .memReadReady(dmem_ready), 
+                        .memWriteDone(dmem_done),
+                        .memDout(dmem_dout),
+                        
+                        // pipeline outputs
+                        .stall(dcache_stall),
+                        .dout(dcache_output),
+
+                        // cache outputs
+                        .cacheEn(DcacheEn),
+                        .cacheWen(DcacheWen),
+                        .cacheMemWen(DcacheMemWen),
+                        .cacheBytesAccess(DcacheBytesAccess),
+                        .cacheBlockAddr(DcacheBlockAddr),
+                        .cacheDin(DcacheDin),
+                        
+                        // memory outputs
+                        .memRen(dmem_ren), 
+                        .memWen(dmem_wen),
+                        .memBlockAddr(dmem_block_address),
+                        .memDin(dmem_din));
 endmodule
--- src/pipelined/toplevel.v
+++ src/pipelined/toplevel.v
@@ -2,17 +2,54 @@
 `include "config.vh"
 
 module toplevel(input clock, 
-				input reset, 
-				output overflow);
+				input reset);
+				
+
+wire    dmem_ren, dmem_wen, dmem_ready, dmem_done;
+wire [] dmem_block_address;
+wire [] dmem_din;
+wire [] dmem_dout;
+
+wire    imem_ren, imem_ready;
+wire [] imem_block_address;
+wire [] imem_dout;
+
 
 cpu cpu(.clock(clock), 
-		.reset(reset), 
-		.overflow(overflow));
+		.reset(reset),
+		//imem
+        .imem_ren(imem_ren),
+        .imem_block_address(imem_block_address),
+        .imem_ready(imem_ready),
+        .imem_dout(imem_dout),
+        //dmem
+		.dmem_ren(dmem_ren), 
+        .dmem_wen(dmem_wen),
+        .dmem_block_address(dmem_block_address), 
+        .dmem_din(dmem_din),
+        .dmem_ready(dmem_ready), 
+        .dmem_done(dmem_done),
+        .dmem_dout(dmem_dout));
+        
 
 // Memories
 
 // to be implemented !!!!!!!!!!!!!!!
-Imem Instruction_Mem();
-Dmem Data_Mem();
+Imem Instruction_Mem(clock(clock), 
+            .reset(reset), 
+			.ren(imem_ren), 
+			.block_address(imem_block_address),
+			.ready(imem_ready), 
+            .dout(imem_dout));
+            
+Dmem Data_Mem(.clock(clock), 
+            .reset(reset), 
+			.ren(dmem_ren), 
+			.wen(dmem_wen),
+			.block_address(dmem_block_address), 
+			.din(dmem_din),
+			.ready(dmem_ready), 
+			.done(dmem_done),
+            .dout(dmem_dout));
 
 endmodule
