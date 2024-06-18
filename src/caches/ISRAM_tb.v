d29f3113191a3d6a65b6729b344c09b5c6ad7c34
AnnaGais <annagaisidou1@gmail.com>
2024-06-19 01:50:29 +0300

    Update ISRAM_tb.v

--- src/caches/ISRAM_tb.v
+++ src/caches/ISRAM_tb.v
@@ -3,17 +3,17 @@
 
 module Icache_SRAM_tb;
     reg clk, rst;
-    reg en, memWen;
+    reg ren, memWen;
     reg [`ITAG_SIZE+`ISET_INDEX_SIZE-1:0] blockAddr;
     reg [`IBLOCK_SIZE_BITS-1:0] dataIn;
 
     wire hit;
     wire [`IBLOCK_SIZE_BITS-1:0] dataOut;
 
-    Icache_SRAM I_SRAM_inst(.clk(clk), .rst(rst), .en(en), .memWen(memWen), .blockAddr(blockAddr), .dataIn(dataIn), .hit(hit), .dataOut(dataOut));
+    Icache_SRAM I_SRAM_inst(.clk(clk), .rst(rst), .ren(ren), .memWen(memWen), .blockAddr(blockAddr), .dataIn(dataIn), .hit(hit), .dataOut(dataOut));
 
     initial begin
-        en=0;
+       ren=0;
         memWen=0;
         clk=0;
         rst=0;
@@ -24,7 +24,7 @@ module Icache_SRAM_tb;
         blockAddr={`ITAG_SIZE+`ISET_INDEX_SIZE{1'b0}};
         dataIn={`IBLOCK_SIZE_BITS{1'b0}};
 
-        en=1;
+        ren=0;
         memWen=1;
         
         //WRITE DIFFERENT TAG / TARGETTING SET 0 BLOCK 1//
@@ -32,7 +32,6 @@ module Icache_SRAM_tb;
         blockAddr={{`ITAG_SIZE{1'b1}},{`ISET_INDEX_SIZE{1'b0}}};
         dataIn={`IBLOCK_SIZE_BITS{1'b1}};
 
-        en=1;
         memWen=1;
 
         //WRITE TARGETTING SET 1 BLOCK 1//
@@ -40,7 +39,6 @@ module Icache_SRAM_tb;
         blockAddr <= {{`ITAG_SIZE{1'b1}},{`ISET_INDEX_SIZE{1'b0}}};
         dataIn <= {{32{1'b1}},{`IBLOCK_SIZE_BITS-32{1'b0}}};
 
-        en=1;
         memWen=1;
         
         //WRITE TARGETTING SET 1 BLOCK 1//
@@ -48,15 +46,19 @@ module Icache_SRAM_tb;
         blockAddr={{`ITAG_SIZE{1'b1}},{`ISET_INDEX_SIZE{1'b0}}};
         dataIn={{32{1'b0}},{`IBLOCK_SIZE_BITS-32{1'b1}}};
 
-        en=1;
         memWen=1;
       
         //READ TARGETTING SET 1 BLOCK 1//
         #10
         blockAddr={{`ITAG_SIZE{1'b1}},{`ISET_INDEX_SIZE{1'b0}}};
-
-        en=1;
+        ren=1;
         memWen=0;
+        
+         #10
+        
+        blockAddr={`ITAG_SIZE+`ISET_INDEX_SIZE{1'b0}};
+        memWen=0;
+        
            
     end
 
