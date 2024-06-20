205b47c469abe5af865fbee163fe4c810c2fe9d3
D.Voi <129060703+D-Voi@users.noreply.github.com>
2024-06-21 01:48:29 +0300

    some changes done in cpu

--- src/pipelined/cpu.v
+++ src/pipelined/cpu.v
@@ -47,77 +47,73 @@ wire [`DBLOCK_SIZE_BITS-1:0] dcache_input;
 // dcache
 wire DcacheRen, DcacheWen, DcacheMemWen;
 wire [`DBLOCK_SIZE-1:0] DcacheBytesAccess;
-wire [`DTAG_SIZE+`DSET_INDEX_SIZE-1:0] DcacheBlockAddr;
+wire [`DMEM_BLOCK_ADDR_SIZE-1:0] DcacheBlockAddr;
 wire [`DBLOCK_SIZE_BITS-1:0] DcacheDin;
 wire DcacheHit, DcacheDirtyBit;
 wire [`DBLOCK_SIZE_BITS-1:0] DcacheDout;
 
 // icache
 wire IcacheRen, IcacheMemWen;
-wire [`ITAG_SIZE+`ISET_INDEX_SIZE-1:0] IcacheBlockAddr;
+wire [`IMEM_BLOCK_ADDR_SIZE-1:0] IcacheBlockAddr;
 wire [`IBLOCK_SIZE_BITS-1:0] IcacheDin;
 wire IcacheHit, IcacheDirtyBit;
 wire [`IBLOCK_SIZE_BITS-1:0] IcacheDout;
 
 pipeline pipeline(.clock(clock), 
-				  .reset(reset),
-		/*input*/ .dcache_stall(dcache_stall),
+				          .reset(reset),
+		              .dcache_stall(dcache_stall),
                   .icache_stall(icache_stall),
                   .dcache_output(dcache_output),
-				  .dcache_ren(dcache_ren),
-				  .dcache_wen(dcache_wen),
-				  .dcache_addr(dcache_addr),
-				  .byteSelectVector(byteSelectVector),
-				  .dcache_input(dcache_input));
+				          .dcache_ren(dcache_ren),
+				          .dcache_wen(dcache_wen),
+				          .dcache_addr(dcache_addr),
+				          .byteSelectVector(byteSelectVector),
+				          .dcache_input(dcache_input));
 
 // caches instantiated here
-I_SRAM Icache(.clk(clock), 
-                .rst(reset),
-                .ren(IcacheRen), 
-                .memWen(IcacheMemWen),
-                .blockAddr(IcacheBlockAddr), 
-                .dataIn(IcacheDin), 
-                .hit(IcacheHit),
-                .dataOut(IcacheDout));
+Icache_SRAM Icache(.clk(clock), 
+                   .rst(reset),
+                   .ren(IcacheRen), 
+                   .memWen(IcacheMemWen),
+                   .blockAddr(IcacheBlockAddr), 
+                   .dataIn(IcacheDin), 
+                   .hit(IcacheHit),
+                   .dataOut(IcacheDout));
 
-D_SRAM Dcache(.clk(clock), 
-                .rst(reset),
-                .ren(DcacheRen), 
-                .wen(DcacheWen), 
-                .memWen(DcacheMemWen),
-                .bytesAccess(DcacheBytesAccess),
-                .blockAddr(DcacheBlockAddr), 
-                .dataIn(DcacheDin), 
-                .hit(DcacheHit),
-                .dirtyBit(DcacheDirtyBit),
-                .dataOut(DcacheDout));
+Dcache_SRAM Dcache(.clk(clock), 
+                   .rst(reset),
+                   .ren(DcacheRen), 
+                   .wen(DcacheWen), 
+                   .memWen(DcacheMemWen),
+                   .bytesAccess(DcacheBytesAccess),
+                   .blockAddr(DcacheBlockAddr), 
+                   .dataIn(DcacheDin), 
+                   .hit(DcacheHit),
+                   .dirtyBit(DcacheDirtyBit),
+                   .dataOut(DcacheDout));
 
-dcache_controller Dcntr(
-  // Instantiate dcache_controller
-    .clock(clock),
-    .reset(reset),
-    .ren(ren),
-    .wen(wen),
-    .addr(addr),
-    .byteSelectVector(byteSelectVector),
-    .din(din),
-    .cacheHit(cacheHit),
-    .cacheDirtyBit(cacheDirtyBit),
-    .cacheDout(cacheDout),
-    .memReadReady(memReadReady),
-    .memWriteDone(memWriteDone),
-    .memDout(memDout),
-    .stall(stall),
-    .dout(dout),
-    .BlockAddr(BlockAddr),
-    .cacheRen(cacheRen),
-    .cacheWen(cacheWen),
-    .cacheMemWen(cacheMemWen),
-    .cacheBytesAccess(cacheBytesAccess),
-    .cacheDin(cacheDin),
-    .memRen(memRen),
-    .memWen(memWen),
-    .memDin(memDin)
-);
-
+dcache_controller Dcntr(.clock(clock),
+                        .reset(reset),
+                        .ren(ren),
+                        .wen(wen),
+                        .addr(addr),
+                        .byteSelectVector(byteSelectVector),
+                        .din(din),
+                        .cacheHit(cacheHit),
+                        .cacheDirtyBit(cacheDirtyBit),
+                        .cacheDout(cacheDout),
+                        .memReadReady(memReadReady),
+                        .memWriteDone(memWriteDone),
+                        .memDout(memDout),
+                        .stall(stall),
+                        .dout(dout),
+                        .BlockAddr(BlockAddr),
+                        .cacheRen(cacheRen),
+                        .cacheWen(cacheWen),
+                        .cacheMemWen(cacheMemWen),
+                        .cacheBytesAccess(cacheBytesAccess),
+                        .cacheDin(cacheDin),
+                        .memRen(memRen),
+                        .memWen(memWen),
+                        .memDin(memDin));
 endmodule
