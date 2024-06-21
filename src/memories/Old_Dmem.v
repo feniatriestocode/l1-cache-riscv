7f0991f633c4125789876e2501c9391d7a782301
D.Voi <129060703+D-Voi@users.noreply.github.com>
2024-06-21 04:44:26 +0300

    old to be safe

---
+++ src/memories/Old_Dmem.v
@@ -0,0 +1,100 @@
+/*IMPLEMENTATION OF A PARAMETRIZED DATA MEMORY ON RISC-V*/
+
+////// `timescale 1ns / 1ps
+//`include "../include/constants.v"
+//`include "../include/constants.vh"
+//`include "../common/counter.v" //sees it from makefile supposedly
+
+// If ren stays up then the next read has no delay !
+
+// Read:  Input: reset = 1'b1, ren = 1'b1, wen = 1'b0, block_address = address of block in memory, din = don't care.
+//        Output: when ready = 1'b1: dout = desired block data, done = don't care.
+// Write: Input: reset = 1'b1, ren = 1'b0, wen = 1'b1, block address = address of block in memory, din = data of block.
+//        Output: ready = don't care, dout = don't care, when done = 1'b1: data is written.
+module Dmem(input clock, reset, 
+			input ren, wen,
+			input [(`DMEM_BLOCK_ADDR_SIZE-1):0] block_address, 		// in blocks
+			input [(`DBLOCK_SIZE_BITS-1):0] din,
+			output reg ready, done,
+			output [(`DBLOCK_SIZE_BITS-1):0] dout);
+
+/****** SIGNALS ******/
+reg [(`DBLOCK_SIZE_BITS-1):0] data [0:(`DMEM_SIZE_BLOCKS-1)];
+reg [(`DBLOCK_SIZE_BITS-1):0] temp_din;
+reg flag;
+
+wire delayed, counter_reset;
+wire [(`DMEM_DELAY_CNTR_SIZE-1):0] delay_counter;
+wire temp_ready, temp_done;
+
+/****** LOGIC ******/
+assign counter_reset = ~reset || (~wen && ~ren) || (wen && ren);
+
+counter #(.size(`DMEM_DELAY_CNTR_SIZE)) delay_cntr (.reset(counter_reset), .clk(clock), .hold(delayed), .counter(delay_counter));
+
+assign delayed = &delay_counter;
+
+assign temp_ready = delayed && ren && ~wen;
+assign temp_done = delayed && ~ren && wen;
+
+always @ (posedge clock or negedge reset)
+begin
+	if(~reset)
+	begin
+		ready <= 1'b0;
+		done <= 1'b0;
+	end
+	else
+	begin
+		ready <= temp_ready;
+		done <= temp_done;
+	end
+end
+
+// read
+assign dout = (temp_ready) ? data[block_address] : {`DBLOCK_SIZE_BITS{1'b0}};
+
+// write
+always @ (posedge clock or negedge reset)
+begin
+	if(~reset)
+	begin
+		temp_din <= {`DBLOCK_SIZE_BITS{1'b0}};
+		flag <= 1'b0;
+	end
+	else
+	begin
+		if(~wen || ren)
+		begin
+			temp_din <= {`DBLOCK_SIZE_BITS{1'b0}};
+			flag <= 1'b0;
+		end
+		else
+		begin
+			if(~flag)
+			begin
+				temp_din <= din;
+				flag <= 1'b1;
+			end
+		end
+	end
+end
+
+always @ (posedge clock)
+begin 
+	if(temp_done)
+	begin
+		data[block_address] <= temp_din;
+	end
+end
+
+/****** SIMULATION ******/
+initial $readmemh("test.hex", data);
+
+always @(ren or wen)
+begin
+	if (ren & wen)
+		$display ("\nMemory ERROR (time %0d): ren and wen both active!\n", $time);
+end
+
+endmodule
---
+++ src/memories/Old_Imem.v
@@ -0,0 +1,49 @@
+/*IMPLEMENTATION OF A PARAMETRIZED INSTRUCTION MEMORY ON RISC-V*/
+
+////// `timescale 1ns / 1ps
+
+//`include "../include/constants.vh"
+//`include "../common/counter.v" //sees it from makefile supposedly
+
+// If ren stays up then the next read has no delay !
+
+// Read: Input: reset = 1'b1, ren = 1'b1, block_address = address of block in memory.
+//       Output: when ready = 1'b1: dout = desired block data.
+// No Write.
+module Imem(input clock, reset, 
+			input ren, 
+			input [(`IMEM_BLOCK_ADDR_SIZE-1):0] block_address, 		// in blocks
+			output reg ready,
+			output [(`IBLOCK_SIZE_BITS-1):0] dout);
+
+/****** SIGNALS ******/
+reg [(`IBLOCK_SIZE_BITS-1):0] data [0:(`IMEM_SIZE_BLOCKS-1)];
+
+wire delayed, counter_reset;
+wire [(`IMEM_DELAY_CNTR_SIZE-1):0] delay_counter;
+
+/****** LOGIC ******/
+assign counter_reset = ~reset || ~ren;
+
+counter #(.size(`IMEM_DELAY_CNTR_SIZE)) delay_cntr (.reset(counter_reset), .clk(clock), .hold(delayed), .counter(delay_counter));
+
+assign delayed = &delay_counter;
+
+always @ (posedge clock or negedge reset)
+begin
+	if(~reset)
+	begin
+		ready <= 1'b0;
+	end
+	else
+	begin
+		ready <= delayed;
+	end
+end 
+
+assign dout = delayed ? data[block_address] : {`IBLOCK_SIZE_BITS{1'b0}};
+
+/****** SIMULATION ******/
+initial $readmemh("test.hex", data);
+
+endmodule
